fd058d017c79fb378d3be696791d68d126e39eef
Eric <edhavey@null.net>
2021-09-23 01:14:27 -0500

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution here
+const findMatching = (arrayOfDrivers, name) => {
+  const matches = [];
+  arrayOfDrivers.filter((value) => {
+    if (name.toLowerCase() === value.toLowerCase()) {
+      matches.push(value);
+    }
+  })
+  return matches;
+}
+
+const fuzzyMatch = (arrayOfDrivers, substring) => {
+  const matches = [];
+  arrayOfDrivers.filter((value) => {
+    if (value.toLowerCase().startsWith(substring.toLowerCase())) {
+      matches.push(value);
+    }
+  })
+  return matches;
+};
+
+const matchName = (arrayOfDrivers, name) => {
+  const matches = [];
+  arrayOfDrivers.filter((value) =>{
+    if (value.name === name) {
+      matches.push(value);
+    }
+  })
+  return matches;
+};
